040da906eb6808a03d10a21da7b1bf6972bc7862
Hannes Dorn <hannes@dorn.cc>
2018-11-11 01:44:20 +0100

    modules/process.ps1: formating ws from bytes to MB

--- modules/process.ps1
+++ modules/process.ps1
@@ -1,7 +1,7 @@
-$sTopProcesses = Get-Process -ComputerName $sComputer | Sort WS -Descending | Select ProcessName, Id, WS -First $iProccesses | ConvertTo-Html -Fragment
+$sTopProcesses = Get-Process -ComputerName $sComputer | Sort WS -Descending | Select ProcessName, Id, @{n='Working Set (MB)';e={"{0:n2}" -f ($_.ws/1mb)}} -First $iProccesses | ConvertTo-Html -Fragment
 
 $sContent += @"
     <h3>System Processes - Top $iProccesses Highest Memory Usage</h3>
-    <p>The following $iProccesses processes are those consuming the highest amount of Working Set (WS) Memory (bytes) on $sComputer</p>
+    <p>The following $iProccesses processes are those consuming the highest amount of memory on $sComputer</p>
     $sTopProcesses
 "@
