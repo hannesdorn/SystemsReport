07365ff43e174a4c9ad6570c1548f386e4c3b17f
Hannes Dorn <hannes@dorn.cc>
2018-11-13 00:55:10 +0100

    System: Exchanged free RAM and free Disk with used RAM and used Disk

--- modules/system.ps1
+++ modules/system.ps1
@@ -3,23 +3,23 @@ $SystemInfo = Get-WmiObject -Class Win32_OperatingSystem -computername $sCompute
 $TotalRAM = $SystemInfo.TotalVisibleMemorySize/1MB
 $FreeRAM = $SystemInfo.FreePhysicalMemory/1MB
 $UsedRAM = $TotalRAM - $FreeRAM
-$RAMPercentFree = ($FreeRAM / $TotalRAM) * 100
+$RAMPercent = ($UsedRAM / $TotalRAM) * 100
 $TotalRAM = [Math]::Round($TotalRAM, 2)
 $FreeRAM = [Math]::Round($FreeRAM, 2)
 $UsedRAM = [Math]::Round($UsedRAM, 2)
-$RAMPercentFree = [Math]::Round($RAMPercentFree, 2)
+$RAMPercent = [Math]::Round($RAMPercent, 2)
 $Uptime = HostGetUptime($sComputer)
 
 $oProcessor = Get-WmiObject -computername $sComputer win32_processor | Measure-Object -property LoadPercentage -Average | Select Average
 
 # Create the chart using our Chart Function
-$sRamImage = ChartCreatePie "RAM usage (Used/Free)" "GB" $FreeRAM $UsedRAM
+$sRamImage = ChartCreatePie "RAM usage (Used/Free)" "GB" $UsedRAM $FreeRAM
 
 # Get free space of C:
-$oVolumeC = Get-WmiObject -Class win32_Volume -ComputerName $sComputer -Filter "DriveLetter = 'C:'" | Select-object @{n='percent free'; Expression = {"{0:N2}" -f  (($_.FreeSpace / $_.Capacity) * 100)}}, @{n='Capacity';e={"{0:n2}" -f ($_.capacity/1gb)}}, @{n='FreeSpace';e={"{0:n2}" -f ($_.freespace/1gb)}}
+$oVolumeC = Get-WmiObject -Class win32_Volume -ComputerName $sComputer -Filter "DriveLetter = 'C:'" | Select-object @{n='UsedSpace'; Expression = {"{0:N2}" -f  (($_.Capacity - $_.FreeSpace)/1gb)}}, @{n='Percent'; Expression = {"{0:N2}" -f  ((($_.Capacity - $_.FreeSpace) / $_.Capacity) * 100)}}, @{n='Capacity';e={"{0:n2}" -f ($_.capacity/1gb)}}, @{n='FreeSpace';e={"{0:n2}" -f ($_.freespace/1gb)}}
 
 # Create the chart using our Chart Function
-$sCImage = ChartCreatePie "C usage  (Used/Free)" "GB" $oVolumeC.Freespace $oVolumeC.Capacity
+$sCImage = ChartCreatePie "C usage  (Used/Free)" "GB" $oVolumeC.UsedSpace $oVolumeC.FreeSpace
 
 $sContent += @"
 	<h3>System Info</h3>
@@ -38,11 +38,11 @@ $sContent += @"
         </tr>
         <tr>
             <td>RAM</td>
-            <td>$RAMPercentFree % ($FreeRAM GB) of $TotalRAM GB free</td>
+            <td>$RAMPercent % ($UsedRAM GB) of $TotalRAM GB used</td>
         </tr>
         <tr>
             <td>C</td>
-            <td>$($oVolumeC.'percent free') % ($($oVolumeC.Freespace) GB) of $($oVolumeC.Capacity) GB free</td>
+            <td>$($oVolumeC.'Percent') % ($($oVolumeC.UsedSpace) GB) of $($oVolumeC.Capacity) GB used</td>
         </tr>
 	</table>
 
