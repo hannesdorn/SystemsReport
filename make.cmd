fd5b4c0aa5ac3b307de3169a7c66411c0a123657
Hannes Dorn <hannes@dorn.cc>
2023-06-06 09:44:34 +0200

    make.cmd: changed zip.exe to 7z.exe

--- make.cmd
+++ make.cmd
@@ -3,5 +3,5 @@ if "%~1" == "" echo "%~nx0 Version, z.B. 0.3.0" & goto :eof
 set filename=releases\SystemsReport-v%1.zip
 
 if exist %filename% del %filename%
-zip.exe %filename% Mailtest.cmd Mailtest.ps1 SystemsMonitor.cmd SystemsMonitor.ps1  SystemsReport.cmd SystemsReport.ps1 config\config-sample.ps1 functions\* modules\* templates\* version.txt
+7z.exe a %filename% Mailtest.cmd Mailtest.ps1 SystemsMonitor.cmd SystemsMonitor.ps1  SystemsReport.cmd SystemsReport.ps1 config\config-sample.ps1 functions\* modules\* templates\* version.txt
 CertUtil -hashfile %filename% sha256 | find /V "SHA256-Hash" | find /V "CertUtil" > %filename%.sha256
